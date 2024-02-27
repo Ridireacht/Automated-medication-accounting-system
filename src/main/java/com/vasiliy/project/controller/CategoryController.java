a220fed577291abf9a6f1a8f9ae11f888879eccd
Ridireacht <ridiredire@gmail.com>
2024-02-27 21:07:15 +0300

    Basic start

--- src/main/java/com/vasiliy/project/controller/CategoryController.java
+++ src/main/java/com/vasiliy/project/controller/CategoryController.java
@@ -1,21 +1,23 @@
 package com.vasiliy.project.controller;
 
+import com.vasiliy.project.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/categories")
 public class CategoryController {
 
+  private final CategoryService categoryService;
+
+
   @GetMapping
   public String getCategories(Model model) {
-    model.addAttribute("pageTitle", "Привет, мир!");
-    model.addAttribute("message", "Это страница, созданная с использованием Thymeleaf.");
+    model.addAttribute("categories", categoryService.getAllCategories());
     return "categories";
   }
 }
