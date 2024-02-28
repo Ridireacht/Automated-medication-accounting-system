516f2fe6d7320cba6ed7e8f9185a302076a2bec8
Ridireacht <ridiredire@gmail.com>
2024-02-28 16:14:28 +0300

    Add create category, delete Liquibase

--- src/main/java/com/vasiliy/project/controller/CategoryController.java
+++ src/main/java/com/vasiliy/project/controller/CategoryController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,12 @@ public class CategoryController {
 
   private final CategoryService categoryService;
 
+
+  @PostMapping
+  public ResponseEntity<String> addCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+    categoryService.addCategory(categoryDTO);
+    return ResponseEntity.ok().body("{}");
+  }
 
   @PutMapping("/{id}")
   public ResponseEntity<String> updateCategory(@PathVariable("id") Long categoryId, @Valid @RequestBody CategoryDTO categoryDTO) {
