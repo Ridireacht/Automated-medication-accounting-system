a3b6606480f6d27cce4474de9fae10cd82a74d0e
Ridireacht <ridiredire@gmail.com>
2024-02-29 20:34:45 +0300

    Fix mapper error

--- src/main/java/com/vasiliy/project/mapper/ProductMapper.java
+++ src/main/java/com/vasiliy/project/mapper/ProductMapper.java
@@ -2,14 +2,20 @@ package com.vasiliy.project.mapper;
 
 import com.vasiliy.project.dto.ProductDTO;
 import com.vasiliy.project.entity.info.Product;
+import com.vasiliy.project.repository.CategoryRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring")
-public interface ProductMapper {
+public abstract class ProductMapper {
+
+  @Autowired
+  protected CategoryRepository categoryRepository;
+
 
   @Mapping(target = "category", expression = "java(categoryRepository.findById(productDTO.getCategoryId()).get())")
-  Product mapDTOtoProduct(ProductDTO productDTO);
+  public abstract Product mapDTOtoProduct(ProductDTO productDTO);
 }
